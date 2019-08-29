8bf617ed2c45d9b9bac8ca4d12c2768e62de937b
BrandPM18 <44950600+BrandPM18@users.noreply.github.com>
2019-08-29 15:32:17 -0500

    Create Nombre.c

---
+++ Programas basicos/Nombre.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main ()
+{
+ char A[30];
+ printf("Coloque su nombre\n"); 
+ scanf("%s",A);                 
+ printf("Tu nombre es %s",A);                
+}
+
+/*  
+OUTPUT
+Coloque su nombre
+INPUT
+BrandPM
+OUTPUT
+Tu nombre es BrandPM
+*/
